cb4e5a22dd782277d2dd4295e01aa9ac76d7e227
mizphses <fuminori@mizphses.com>
2025-05-14 00:39:07 +0900

    route

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
 import { Hono } from "hono";
+import auth from "./routes/auth";
+import admin from "./routes/admin";
+import tanzaku from "./routes/tanzaku";
 
 const app = new Hono<{ Bindings: CloudflareBindings }>();
-
-app.get("/message", (c) => {
+app.get("/", (c) => {
   return c.text("Hello Hono!");
 });
 
+app.route("/auth", auth);
+app.route("/admin", admin);
+app.route("/tanzaku", tanzaku);
 export default app;
