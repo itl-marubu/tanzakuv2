85831195d0927c817d85085bebae0e65f9ff4ab6
mizphses <fuminori@mizphses.com>
2025-05-17 17:28:39 +0900

    remove title from sql

--- migrations/0002_remove-title-of-tanzaku.sql
+++ migrations/0002_remove-title-of-tanzaku.sql
@@ -1,1 +1,15 @@
--- Migration number: 0002 	 2025-05-17T08:27:26.147Z
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Tanzaku" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "content" TEXT NOT NULL,
+    "userName" TEXT NOT NULL,
+    "visiblePattern" BOOLEAN NOT NULL DEFAULT true,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+INSERT INTO "new_Tanzaku" ("content", "createdAt", "id", "userName", "visiblePattern") SELECT "content", "createdAt", "id", "userName", "visiblePattern" FROM "Tanzaku";
+DROP TABLE "Tanzaku";
+ALTER TABLE "new_Tanzaku" RENAME TO "Tanzaku";
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
