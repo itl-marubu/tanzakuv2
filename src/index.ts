b02591db8904baad9cd2c53c348d731a04d96642
mizphses <fuminori@mizphses.com>
2025-05-17 17:42:24 +0900

    cors

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,13 @@ import { Hono } from "hono";
 import admin from "./routes/admin";
 import auth from "./routes/auth";
 import tanzaku from "./routes/tanzaku";
+import { cors } from "hono/cors";
 
 const app = new Hono<{ Bindings: CloudflareBindings }>();
 app.get("/", (c) => {
   return c.text("Hello Hono!");
 });
+app.use("*", cors());
 
 app.route("/auth", auth);
 app.route("/admin", admin);
